9c3a7ba132058059117fb29b8f96c84c1eddbe06
Scarjit <scarjit@aol.com>
2019-10-18 07:34:11 +0200

    Changed magic number to EPSILON/2

--- tests/mod.rs
+++ tests/mod.rs
@@ -6,13 +6,15 @@ mod tests{
     use uoi::conversion::hartley;
     use uoi::conversion::nat;
     use uoi::conversion::trit;
+    use std::f64::EPSILON;
 
+    const MAX_FLOAT_ROUNDING_DEVIATION: f64 = EPSILON/2.0;
     #[test]
     fn test_hartley_nat_hartley(){
         let hartley: f64 = 1.0;
         let nat = hartley::to_nat(hartley);
         let hartley_out = nat::to_hartley(nat);
-        assert!(hartley-hartley_out <= 0.00000000000000011102230246251565);
+        assert!(hartley-hartley_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
     #[test]
@@ -20,7 +22,7 @@ mod tests{
         let nat: f64 = 1.0;
         let hartley = nat::to_hartley(nat);
         let nat_out = hartley::to_nat(hartley);
-        assert!(nat-nat_out <= 0.00000000000000011102230246251565);
+        assert!(nat-nat_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
     #[test]
@@ -28,7 +30,7 @@ mod tests{
         let bit: f64 = 1.0;
         let trit = bit::to_trit(bit);
         let bit_out = trit::to_bit(trit);
-        assert!(bit-bit_out <= 0.00000000000000011102230246251565);
+        assert!(bit-bit_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -37,7 +39,7 @@ mod tests{
         let bit: f64 = 1.0;
         let hartley = bit::to_hartley(bit);
         let bit_out = hartley::to_bit(hartley);
-        assert!(bit-bit_out <= 0.00000000000000011102230246251565);
+        assert!(bit-bit_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -46,7 +48,7 @@ mod tests{
         let bit: f64 = 1.0;
         let nat = bit::to_nat(bit);
         let bit_out = nat::to_bit(nat);
-        assert!(bit-bit_out <= 0.00000000000000011102230246251565);
+        assert!(bit-bit_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -55,7 +57,7 @@ mod tests{
         let trit: f64 = 1.0;
         let nat = trit::to_nat(trit);
         let trit_out = nat::to_trit(nat);
-        assert!(trit-trit_out <= 0.00000000000000011102230246251565);
+        assert!(trit-trit_out <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
     #[test]
@@ -63,7 +65,9 @@ mod tests{
         let s1: StorageUnit = StorageUnit::new(Some(1.0), None, None, None);
         let s2: StorageUnit = StorageUnit::new(None, Some(1.0), None, None);
         let s3: StorageUnit = s1 + s2;
-        assert_eq!(s3.get_bits(), 1.0 + hartley::to_bit(1.0));
+        //s3.get_bits(), 1.0 + hartley::to_bit(1.0)
+
+        assert!(s3.get_bits() - (1.0 + hartley::to_bit(1.0)) <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -72,7 +76,8 @@ mod tests{
         let s1: StorageUnit = StorageUnit::new(Some(1.0), None, None, None);
         let s2: StorageUnit = StorageUnit::new(None, Some(1.0), None, None);
         let s3: StorageUnit = s1 - s2;
-        assert_eq!(s3.get_bits(), 1.0 - hartley::to_bit(1.0));
+
+        assert!(s3.get_bits() - ( 1.0 - hartley::to_bit(1.0)) <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -81,7 +86,7 @@ mod tests{
         let s1: StorageUnit = StorageUnit::new(Some(1.0), None, None, None);
         let s2: StorageUnit = StorageUnit::new(None, Some(1.0), None, None);
         let s3: StorageUnit = s1 * s2;
-        assert_eq!(s3.get_bits(), 1.0 * hartley::to_bit(1.0));
+        assert!(s3.get_bits() - ( 1.0 * hartley::to_bit(1.0)) <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 
 
@@ -90,6 +95,6 @@ mod tests{
         let s1: StorageUnit = StorageUnit::new(Some(1.0), None, None, None);
         let s2: StorageUnit = StorageUnit::new(None, Some(1.0), None, None);
         let s3: StorageUnit = s1 / s2;
-        assert_eq!(s3.get_bits(), 1.0 / hartley::to_bit(1.0));
+        assert!(s3.get_bits() - ( 1.0 / hartley::to_bit(1.0)) <= MAX_FLOAT_ROUNDING_DEVIATION);
     }
 }
